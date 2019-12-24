ad817d827d85737723ce9236a625f1a133674c7b
Maxwell Spangler <maxwell@maxwellspangler.com>
2019-12-24 02:23:51 -0700

    Deploy work.desktop file along with position script

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 echo "Deploy position to /usr/local/bin"
-sudo cp -prv position /usr/local/bin
+sudo cp -pr position /usr/local/bin
 sudo chmod a+rx /usr/local/bin/position
+
+echo "Deploy GNOME desktop shortcut to call position"
+sudo cp -pr work.desktop /usr/share/applications
